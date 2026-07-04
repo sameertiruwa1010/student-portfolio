82cd255319e76af77763dbb855dbfa571bf475d9
sameer <sameertiruwa1010@gmail.com>
2026-07-04 22:13:52 +0545

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,8 @@ function Navbar() {
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#contact">Blog</a></li>
+
       </ul>
 
       <a
